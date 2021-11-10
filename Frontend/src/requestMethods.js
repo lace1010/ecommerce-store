70010ae070ac997be98ea034e144eac45a449eaf
Hunter Lacefield <lace1010@gmail.com>
2021-11-11 00:42:55 +0200

    made baseURL value inside .env variable to keep app more secure

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://backend-ecommerce-store.herokuapp.com/api/";
+const BASE_URL = process.env.REACT_APP_HEROKU_URL;
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxODNhZWQ2MGY4ZjIyOWJkNjk4MjIwYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjMwNTk0NSwiZXhwIjoxNjM2NTY1MTQ1fQ.AwJ-TVBHgCtGvYcRKbNjuRVE1KVg_6jn9v5fuayfPp4";
 
