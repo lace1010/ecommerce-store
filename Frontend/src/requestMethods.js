ee650470383c31612b0aab13d506253959e6decc
Hunter Lacefield <lace1010@gmail.com>
2021-11-11 19:24:59 +0200

    updated frontendurl

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -4,7 +4,7 @@ const BASE_URL = process.env.REACT_APP_HEROKU_URL;
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxODNhZWQ2MGY4ZjIyOWJkNjk4MjIwYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjMwNTk0NSwiZXhwIjoxNjM2NTY1MTQ1fQ.AwJ-TVBHgCtGvYcRKbNjuRVE1KVg_6jn9v5fuayfPp4";
 
-export const frontEndUrl = "http://localhost:3000";
+export const frontEndUrl = "https://ecommerce-store-furniture.netlify.app";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
